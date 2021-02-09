a3949e83f879e9aa5d3d2598270862189556b4ea
Your Username <you@example.com>
2021-02-09 12:22:04 -0500

    commit tree f447bf58307ab12d0c11664eee95c926ba8cd7a6

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ActiveRecord::Base
-  validates_presence_of :name, :email, :password
+  validates_presence_of :name, :email, :password 
+
+  Create Table users ( 
+    name TEXT, 
+    email TEXT, 
+    password TEXT
+  );
 end
